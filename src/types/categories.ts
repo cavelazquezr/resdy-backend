2b1a39e472d2837666db5164e04302289d027b13
cavelazquezr <95.carlos.velazquez@gmail.com>
2024-05-04 12:09:48 +0200

    several features and fixes

--- src/types/categories.ts
+++
@@ -1,7 +0,0 @@
-import { Category } from "@prisma/client";
-import { WithHide } from ".";
-
-export type CategoryOutput = Category;
-export type CategoryProps = Partial<Category>;
-export type CategoryCreateInput = Pick<Category, "label">;
-export type CategoryUpdateInput = WithHide<Partial<Pick<CategoryProps, "label">>>;
--- src/types/menu.ts
+++
@@ -1,6 +0,0 @@
-import { DishProps } from "./dishes";
-
-export interface MenuOutput {
-	category: string;
-	dishes: DishProps[];
-}
